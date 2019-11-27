bec4292a4fd0a3194aa76c6ff3286db8d78de5bc
Bill <castillobil@gmail.com>
2019-11-28 01:20:11 +0800

    redeploy

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import fetch from 'isomorphic-unfetch';
-import Page from '../sections/page';
-import Menu from '../sections/menu';
+import React from "react";
+import fetch from "isomorphic-unfetch";
+import Page from "../sections/page";
+import Menu from "../sections/menu";
 
 /**
  * TODOs:
@@ -21,111 +21,113 @@ import Menu from '../sections/menu';
 // })
 
 const url =
-	'https://spreadsheets.google.com/feeds/list/1UjD-AjOJr-CaYSbeNjltDZCX3B10ysVRvAF5pXDikUg/1/public/values?alt=json';
+  "https://spreadsheets.google.com/feeds/list/1UjD-AjOJr-CaYSbeNjltDZCX3B10ysVRvAF5pXDikUg/1/public/values?alt=json";
 
 class Home extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			menuData: null,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuData: null
+    };
+  }
 
-	componentDidMount() {
-		const { menuData } = this.state;
-		fetch(url)
-			.then(response => response.json())
-			.then(response => {
-				console.log('menuData 1:', menuData);
-				this.setState({ menuData: response.feed.entry });
-			});
-	}
+  componentDidMount() {
+    const { menuData } = this.state;
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        console.log("menuData 1:", menuData);
+        this.setState({ menuData: response.feed.entry });
+      });
+  }
 
-	// static async getInitialProps() {
-	//   const spreadsheetURL = 'https://spreadsheets.google.com/feeds/list/1UjD-AjOJr-CaYSbeNjltDZCX3B10ysVRvAF5pXDikUg/1/public/values?alt=json'
-	//   const res = await fetch(spreadsheetURL)
-	//   const menuData = await res.json()
+  // static async getInitialProps() {
+  //   const spreadsheetURL = 'https://spreadsheets.google.com/feeds/list/1UjD-AjOJr-CaYSbeNjltDZCX3B10ysVRvAF5pXDikUg/1/public/values?alt=json'
+  //   const res = await fetch(spreadsheetURL)
+  //   const menuData = await res.json()
 
-	//   return menuData.feed
-	// }
+  //   return menuData.feed
+  // }
 
-	render() {
-		const { menuData } = this.state;
+  render() {
+    const { menuData } = this.state;
 
-		return (
-			<Page pageTitle="Home">
-				<div className="main-background">{/* <img src="/static/drops2x.png" /> */}</div>
+    return (
+      <Page pageTitle="Home">
+        <div className="main-background">
+          {/* <img src="/static/drops2x.png" /> */}
+        </div>
 
-				<section className="main-section">
-					<h1 className="main-text">
-						<span className="sugarrush">Sugarrush</span>
-						<span className="by-ana">by ana</span>
-					</h1>
-				</section>
+        <section className="main-section">
+          <h1 className="main-text">
+            <span className="sugarrush">Sugarrush</span>
+            <span className="by-ana">by ana</span>
+          </h1>
+        </section>
 
-				<section className="menu-section">
-					<Menu data={menuData} />
-				</section>
+        <section className="menu-section">
+          <Menu data={menuData} />
+        </section>
 
-				<style jsx>{`
-					.main-background {
-						position: absolute;
-						top: 0;
-						right: 0;
-						width: 100%;
-						height: 100%;
-						z-index: -99999;
-						background: url('/static/drops2x.png') no-repeat;
-						background-size: auto 120%;
-						background-position: top right;
-					}
-					.main-background img {
-						width: 100%;
-						transform: scale(0.8);
-						transform-origin: top right;
-					}
-					.main-section {
-						position: relative;
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						min-height: 60vh;
-					}
-					.main-text {
-						text-align: center;
-						text-transform: uppercase;
-					}
-					.main-text span {
-						display: block;
-					}
-					.main-text .sugarrush {
-						font-size: calc(2em + 8vw);
-						-webkit-text-stroke: 2px white;
-						text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.38);
-						letter-spacing: -0.075em;
-						line-height: 0.8;
-						color: #d29ffb;
-					}
-					.main-text .by-ana {
-						font-size: 2em;
-					}
+        <style jsx>{`
+          .main-background {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 100%;
+            height: 100%;
+            z-index: -99999;
+            background: url("/static/drops2x.png") no-repeat;
+            background-size: auto 120%;
+            background-position: top right;
+          }
+          .main-background img {
+            width: 100%;
+            transform: scale(0.8);
+            transform-origin: top right;
+          }
+          .main-section {
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 60vh;
+          }
+          .main-text {
+            text-align: center;
+            text-transform: uppercase;
+          }
+          .main-text span {
+            display: block;
+          }
+          .main-text .sugarrush {
+            font-size: calc(2em + 8vw);
+            -webkit-text-stroke: 2px white;
+            text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.38);
+            letter-spacing: -0.075em;
+            line-height: 0.8;
+            color: #d29ffb;
+          }
+          .main-text .by-ana {
+            font-size: 2em;
+          }
 
-					@media (min-width: 1440px) {
-						.main-text .sugarrush {
-							-webkit-text-stroke: 4px white;
-							text-shadow: 4px 4px 6px rgba(0, 0, 0, 0.38);
-						}
-					}
+          @media (min-width: 1440px) {
+            .main-text .sugarrush {
+              -webkit-text-stroke: 4px white;
+              text-shadow: 4px 4px 6px rgba(0, 0, 0, 0.38);
+            }
+          }
 
-					@media (max-width: 480px) {
-						.main-text .sugarrush {
-							-webkit-text-stroke: 1px white;
-						}
-					}
-				`}</style>
-			</Page>
-		);
-	}
+          @media (max-width: 480px) {
+            .main-text .sugarrush {
+              -webkit-text-stroke: 1px white;
+            }
+          }
+        `}</style>
+      </Page>
+    );
+  }
 }
 
 export default Home;
