29a2578e6b745c20d9fcdbfd5ad650625ddf1889
Bill <castillobil@gmail.com>
2019-11-28 01:27:53 +0800

    fix errors

--- components/menuItem.js
+++ components/menuItem.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { colors, media, fonts } from '../data/theme';
+import React from "react";
+import { colors, media, fonts } from "../data/theme";
 
 /**
  * Returns an object to correspond variants with price
@@ -7,107 +7,114 @@ import { colors, media, fonts } from '../data/theme';
  * @param {Array} price
  */
 const getVariants = (variants, price) => {
-	let newVariants = {};
-	// const entries = new Map([variants, price]);
-	// newVariants = Object.fromEntries(entries);
+  let newVariants = {};
+  // const entries = new Map([variants, price]);
+  // newVariants = Object.fromEntries(entries);
 
-	// Create new object from two arrays: variants and price
-	// Also removes the first space
-	newVariants = variants.map((name, index) => {
-		return {
-			name: name.replace(/\s/, ''),
-			price: parseInt(price[index].replace(/\s/, ''), 10),
-		};
-	});
+  // Create new object from two arrays: variants and price
+  // Also removes the first space
+  newVariants = variants.map((name, index) => {
+    return {
+      name: name.replace(/\s/, ""),
+      price: parseInt(price[index].replace(/\s/, ""), 10)
+    };
+  });
 
-	return newVariants;
+  return newVariants;
 };
 
 const MenuItem = props => {
-	const { data } = props;
-	const variants = getVariants(data.gsx$variants.$t.split(','), data.gsx$price.$t.split(','));
+  const { data } = props;
+  const variants = getVariants(
+    data.gsx$variants.$t.split(","),
+    data.gsx$price.$t.split(",")
+  );
 
-	return (
-		<div className="menu-item">
-			<div className="menu-item-container">
-				{data.gsx$flags.$t !== '' ? (
-					<div className="menu-item-flags">
-						<span className="menu-item-flags-name">{data.gsx$flags.$t}</span>
-					</div>
-				) : (
-					''
-				)}
+  return (
+    <div className="menu-item">
+      <div className="menu-item-container">
+        {data.gsx$flags.$t !== "" ? (
+          <div className="menu-item-flags">
+            <span className="menu-item-flags-name">{data.gsx$flags.$t}</span>
+          </div>
+        ) : (
+          ""
+        )}
 
-				<h4>{data.gsx$menu.$t}</h4>
+        <h4>{data.gsx$menu.$t}</h4>
 
-				{variants.map((variant, index) => (
-					<div className="variants-section" key={index}>
-						<h5 className="variants-name">— {variant.name}</h5>
-						<p className="variants-price">{variant.price}</p>
-					</div>
-				))}
-			</div>
+        {variants.map((variant, index) => (
+          <div className="variants-section" key={index}>
+            <h5 className="variants-name">— {variant.name}</h5>
+            <p className="variants-price">{variant.price}</p>
+          </div>
+        ))}
+      </div>
 
-			<style jsx>{`
-				.menu-item {
-					width: 45%;
-				}
+      <style jsx>{`
+        .menu-item {
+          width: 45%;
+        }
 
-				.menu-item-container {
-					padding: 16px;
-				}
+        .menu-item-container {
+          padding: 16px;
+        }
 
-				.menu-item-flags {
-					padding-bottom: 8px;
-				}
+        .menu-item-flags {
+          padding-bottom: 8px;
+        }
 
-				.menu-item-flags-name {
-					font-size: 12px;
-					font-weight: ${fonts.nunito900};
-					color: ${colors['Angy Red']};
-					text-transform: uppercase;
+        .menu-item-flags-name {
+          font-size: 12px;
+          font-weight: ${fonts.nunito900};
+          color: ${colors["Angy Red"]};
+          text-transform: uppercase;
 
-					padding: 2px 8px;
-					border-radius: 8px;
-					background-color: ${colors['Just Blue']};
-				}
+          padding: 2px 8px;
+          border-radius: 8px;
+          background-color: ${colors["Just Blue"]};
+        }
 
-				.menu-item h4 {
-					font-family: ${fonts.nunito};
-					font-size: 28px;
-					font-weight: ${fonts.nunito900};
-					line-height: 1.4;
-					text-transform: uppercase;
-				}
+        .menu-item h4 {
+          font-family: ${fonts.nunito};
+          font-size: 28px;
+          font-weight: ${fonts.nunito900};
+          line-height: 1.4;
+          text-transform: uppercase;
+        }
 
-				.variants-section {
-					display: flex;
-					justify-content: space-between;
-					line-height: 1.4;
-				}
+        .variants-section {
+          display: flex;
+          justify-content: space-between;
+          line-height: 1.4;
+        }
 
-				.variants-section .variants-name {
-					font-family: ${fonts.nunito};
-					font-size: 28px;
-				}
+        .variants-section .variants-name {
+          font-family: ${fonts.nunito};
+          font-size: 28px;
+        }
 
-				.variants-section .variants-price {
-					font-size: 24px;
-					font-weight: ${fonts.nunito900};
-				}
+        .variants-section .variants-price {
+          font-size: 24px;
+          font-weight: ${fonts.nunito900};
+        }
 
-				@media (max-width: ${media.sm}) {
-					.menu-item {
-						width: 100%;
-					}
+        @media (max-width: ${media.sm}) {
+          .menu-item {
+            width: 100%;
+          }
 
-					.menu-item-container {
-						padding: 0 0 24px 0;
-					}
-				}
-			`}</style>
-		</div>
-	);
+          .menu-item-container {
+            padding: 0 0 32px 0;
+          }
+
+          .menu-item-container {
+            padding: 0 0 24px 0;
+          }
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default MenuItem;
--- sections/page.js
+++ sections/page.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import Link from 'next/link';
-import Head from '../components/Head';
-import GlobalStyles from '../components/globalStyles';
-import OfflineSupport from '../components/OfflineSupport';
-import Footer from '../components/Footer';
+import React from "react";
+import Link from "next/link";
+import Head from "../components/Head";
+import GlobalStyles from "../components/globalStyles";
+import OfflineSupport from "../components/OfflineSupport";
+import Footer from "../components/Footer";
 
 const Page = ({ children, noHeader, pageTitle }) => {
-	return (
-		<>
-			<OfflineSupport />
-			<Head title={pageTitle} />
+  return (
+    <>
+      <OfflineSupport />
+      <Head title={pageTitle} />
 
-			{noHeader ? (
-				''
-			) : (
-				<header>
-					<nav>
-						<ul>
-							<li>
-								<Link href="/">
-									<a>Home</a>
-								</Link>
-							</li>
-						</ul>
-					</nav>
-				</header>
-			)}
+      {noHeader ? (
+        ""
+      ) : (
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      )}
 
-			<main>
-				<GlobalStyles />
-				{children}
-			</main>
+      <main>
+        <GlobalStyles />
+        {children}
+      </main>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 };
 
 export default Page;
