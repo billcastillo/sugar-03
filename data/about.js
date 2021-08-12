9aea2083d30a63d759b7795b14b19b500f270884
Bill <castillobil@gmail.com>
2021-08-12 15:03:41 +0800

    sample change

--- data/about.js
+++ data/about.js
@@ -1,4 +1,4 @@
 export const about = {
 	description:
-		"This isn’t your ordinary pastry shop. We bake pastries, and sweets with love. No factories with sweaty men—all confectioneries are homemade gourmet baked to perfection. Imbued with a befitting sweetness you can enjoy either as an appetizer or dessert!"
+		"This isn’t your ordinary pastry shop! We bake pastries, and sweets with love. No factories with sweaty men—all confectioneries are homemade gourmet baked to perfection. Imbued with a befitting sweetness you can enjoy either as an appetizer or dessert!"
 };
